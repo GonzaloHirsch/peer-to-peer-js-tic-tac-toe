5d7a737b889dc190457ccb63e070f39376e6c826
GonzaloHirsch <hirschgonzalo@gmail.com>
2023-11-19 11:44:52 +0000

    fix: pinned module version by SHA

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Infrastructure for the static website
 module "website" {
-  source             = "github.com/GonzaloHirsch/terraform-infrastructure/static-website"
+  source             = "git::https://github.com/GonzaloHirsch/terraform-infrastructure//static-website?ref=b35ec5244e2ebac66deb7595f634a188073c238e"
   tag_app            = local.app_tag
   app_url            = local.app_tag
   aws_hosted_zone_id = var.hosted_zone_id
