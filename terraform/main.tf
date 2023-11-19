ac98c2f12c29a5e8be4d836fd6f66e3bba1b169d
GonzaloHirsch <hirschgonzalo@gmail.com>
2023-11-19 11:52:54 +0000

    fix: included missing server info removal

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Infrastructure for the static website
 module "website" {
-  source             = "git::https://github.com/GonzaloHirsch/terraform-infrastructure//static-website?ref=b35ec5244e2ebac66deb7595f634a188073c238e"
+  source             = "git::https://github.com/GonzaloHirsch/terraform-infrastructure//static-website?ref=99923841c1883da90517b6085069a3c10b9b53fb"
   tag_app            = local.app_tag
   app_url            = local.app_tag
   aws_hosted_zone_id = var.hosted_zone_id
